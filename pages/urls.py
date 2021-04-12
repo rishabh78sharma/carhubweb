46005b1bbb46c4956b5eb634bcf2d2472e6dda83
rishabh78sharma <rishabhssharma2112@gmail.com>
2021-04-12 09:59:08 +0530

    create pages and set up links

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name= 'home'),
+    path('about', views.about, name = 'about'),
+    path('servies', views.service, name = 'service'),
+    path('contact', views.contact, name = 'contact')
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 def home(request):
    return render(request, 'pages/home.html')
 
+
+def about(request):
+   return render(request, 'pages/about.html')
+
+
+def service(request):
+   return render(request, 'pages/service.html')
+
+
+def contact(request):
+   return render(request, 'pages/contact.html')
